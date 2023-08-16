06142adabbb81dc21ce79732c48ca9b503c70a59
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-08-16 18:16:07 -0500

    Working through studio counting-characters.

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/CharacterCounts.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterCounts {
+
+    public static void main(String[] args){
+
+        String snippet = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+
+        char[] charactersInString = snippet.toCharArray();
+
+        HashMap<Character, Integer> charCounter = new HashMap();
+
+        for(char character : charactersInString){
+            if(charCounter.containsKey(character)){
+                charCounter.put(character, charCounter.get(character) +1);
+            } else {
+                charCounter.put(character, 1);
+            }
+        }
+
+        for(Map.Entry<Character, Integer> entry : charCounter.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+}
