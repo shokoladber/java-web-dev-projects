9f0caa499871eb65dc9d48488ead9b5aa270a370
Michael Allen <93445521+shokoladber@users.noreply.github.com>
2023-08-21 12:00:23 -0500

    Ch4 Exercises.

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Course {
+
+    private String Teacher;
+    private String description;
+    private ArrayList<Student> students = new ArrayList<>();
+
+}
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,8 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+
+        Student michaelAllen = new Student("Michael Allen", 1,  3, 4.0);
+
     }
 }
